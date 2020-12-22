7a4260a0bf847c42523015853187ff0ede49ea1e
sathiraju <sathiraju.kk@gmail.com>
2020-12-22 14:24:52 +0530

    added employee update chage

--- spring-boot-api-tutorial/src/main/java/com/satya/demo/employee/controller/EmployeeController.java
+++ spring-boot-api-tutorial/src/main/java/com/satya/demo/employee/controller/EmployeeController.java
@@ -21,39 +21,36 @@ import com.satya.demo.employee.exception.ResourceNotFoundException;
 import com.satya.demo.employee.model.Employee;
 import com.satya.demo.employee.repository.EmployeeRepository;
 
-@RequestMapping("/api")
+@RequestMapping("/api/emp")
 @RestController
 public class EmployeeController {
+	
 	@Autowired
 	private EmployeeRepository repository;
 
-	// /api/employee/getEmployees
-
-	@GetMapping("/emp/all")
+	@GetMapping("/all")
 	public List<Employee> getEmployees() {
 		return repository.findAll();
 	}
 
-	// /
-	@GetMapping("/emp/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Employee> getAllId(@PathVariable(value = "id") long id) throws ResourceNotFoundException {
 		Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found on :: " + id));
 		return ResponseEntity.ok().body(employee);
 	}
 
-	@PostMapping("/emp")
+	@PostMapping("/")
 	public Employee createUser(@Valid @RequestBody Employee employee) {
+		
+		System.out.println("emp Post Method: "+employee);
 		return repository.save(employee);
 	}
 	
-	@PutMapping("/emp/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity update(@Valid @PathVariable(value = "id") Long id, @RequestBody Employee employee) throws ResourceNotFoundException {
 		
-		System.out.println(" Update Emp Object: ");
-		
-		
+		System.out.println(" Update Emp Object: "+id+" emp :"+employee);
 		Employee emp = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found on :: " + id));
-		
 		emp.setName("updated"+employee.getName());
 		emp.setEmail("updated"+employee.getEmail());
 	    repository.save(emp);
@@ -62,10 +59,7 @@ public class EmployeeController {
 	}
 	
 	
-	
-	
-	
-	  @DeleteMapping("/emp/{id}")
+	  @DeleteMapping("/{id}")
 	  public Map<String, Boolean> delete(@PathVariable(value = "id") Long empId) throws Exception {
 	    Employee emp =repository.findById(empId).orElseThrow(() -> new ResourceNotFoundException("Employee not found on :: " + empId));
 	    repository.delete(emp);
